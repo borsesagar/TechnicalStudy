e609c19530e653ccb80d46cc780b79cd1ff31b84
root <root@localhost.localdomain>
2015-01-15 13:46:49 +0530

    added static library(SameDir) complete code

--- Library/Static/SameDir/Main.c
+++ Library/Static/SameDir/Main.c
@@ -1,1 +1,10 @@
 #include<stdio.h>
+#include "StaticLib.h"
+
+int main(void)
+{
+	printf("In main.c\n");
+	printf("Calling CallLib() from main.c\n");
+	CallLib();
+	return 0;
+}
---
+++ Library/Static/SameDir/StaticLib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void CallLib(void)
+{
+	printf("Calling CallLib() from Static Library\n");
+}
