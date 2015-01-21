c62238cb00486009a00b7ff4e9fa0d0d39d59747
root <root@localhost.localdomain>
2015-01-21 14:28:35 +0530

    Signal implemented

--- Signal/Signal.c
+++ Signal/Signal.c
@@ -1,1 +1,30 @@
-#
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void sigHandler(int sig)
+{
+	char c = '\0';
+	signal(sig, SIG_IGN); //ignore the signal
+	printf("\nCtrl-C pressed!!\n");
+	printf("Do you really want to quite [y/n]?\n");
+	c = getchar();
+
+	if (c =='y' || c == 'Y')
+		exit(0);
+	else
+		signal(SIGINT, sigHandler); //reinstall the signal handler
+}
+
+int main(void)
+{
+	if (signal(SIGINT, SIG_IGN) != SIG_IGN)
+		signal(SIGINT, sigHandler);
+
+	while(1);
+		sleep(1);
+	return 0;
+}
+
+
